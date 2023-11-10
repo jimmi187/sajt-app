423e80dc4012cebf60a3e5bf65357dc6c3170ba8
jimmy187 <project.jimi@proton.me>
2023-11-10 02:00:05 +0100

    .

--- front/src/pages/Recipes.jsx
+++ front/src/pages/Recipes.jsx
@@ -4,25 +4,26 @@ import axios from "axios";
 import { generatePath } from "react-router-dom";
 
 
-function Cart({  updateShoppingList, listOfGroceries, shoppigList }){
-   return(
-      <div>
-         {listOfGroceries !== null ? shoppigList.map(x => <OneProd prod={x} />) : <p>no data</p>}
-      </div>
-   );
-}
-
 
 function OneProd({ prod }) {
    let key = prod.key;
    let x2 = prod.value;
-   // console.log(prod)
-   return (<>
+   return (
+   <>
       <h3>{key} <button> add more </button> </h3>
-      <p> {x2.name}  =======  {x2.price}  ======= {x2.store.map(x=> x+" ") }<button> change </button> <button> remove </button></p> 
-      
+      <p> {x2.name}  =======  {x2.price}  ======= {x2.store.map(x => x + " ")}<button> change </button> <button> remove </button></p>
+
    </>)
 }
+
+function Cart({ updateShoppingList, listOfGroceries, shoppigList }) {
+   return (
+      <div>
+         {listOfGroceries !== null ? shoppigList.map(x => <OneProd prod={x} />) : <li>no data</li>}
+      </div>
+   );
+}
+
 
 
 function Recipes(params) {
@@ -35,18 +36,21 @@ function Recipes(params) {
       axios.get("http://127.0.0.1:4444/yo")
          .then((response) => {
             setGroceries(Object.keys(response.data).map(key => ({ key, value: response.data[key] })));
-            setShoppingList(groceries.map(x => ({key: x.key, value: x.value[0]})));
+            setShoppingList(groceries.map(x => ({ key: x.key, value: x.value[0] })));
          })
+
          .catch((error) => {
             console.log(error);
             setShoppingList(null);
          });
 
-   }, [])
+   }, [groceries])
+
+  
    return (
       <div style={{ color: 'white' }}>
          <h1 >Recepies</h1>
-         <Cart updateShoppingList={setShoppingList} listOfGroceries={groceries} shoppigList={shoppigList}/>
+         <Cart updateShoppingList={setShoppingList} listOfGroceries={groceries} shoppigList={shoppigList} />
       </div>
    )
 }
