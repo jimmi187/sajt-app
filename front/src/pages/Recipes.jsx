4e876ad7b0bfd1ce9957e07684e003b5bb13f635
jimmy187 <project.jimi@proton.me>
2023-11-10 17:07:18 +0100

    fixed Promise fetch

--- front/src/pages/Recipes.jsx
+++ front/src/pages/Recipes.jsx
@@ -9,11 +9,11 @@ function OneProd({ prod }) {
    let key = prod.key;
    let x2 = prod.value;
    return (
-   <>
-      <h3>{key} <button> add more </button> </h3>
-      <p> {x2.name}  =======  {x2.price}  ======= {x2.store.map(x => x + " ")}<button> change </button> <button> remove </button></p>
+      <>
+         <h3>{key} <button> add more </button> </h3>
+         <p> {x2.name}  =======  {x2.price}  ======= {x2.store.map(x => x + " ")}<button> change </button> <button> remove </button></p>
 
-   </>)
+      </>)
 }
 
 function Cart({ updateShoppingList, listOfGroceries, shoppigList }) {
@@ -33,21 +33,28 @@ function Recipes(params) {
 
    useEffect(() => {
       //files
-      axios.get("http://zovinableju.ddns.net:4444/yo") 
+      axios.get("http://zovinableju.ddns.net:4444/yo")
          .then((response) => {
             const groceriesData = Object.keys(response.data).map(key => ({ key, value: response.data[key] }));
             setGroceries(groceriesData);
             setShoppingList(groceriesData.map(x => ({ key: x.key, value: x.value[0] })));
          })
-
-         .catch((error) => {
-            console.log(error);
-            setShoppingList(null);
+         .catch(() => {
+            axios.get("http://localhost:4444/yo")
+               .then((response) => {
+                  const groceriesData = Object.keys(response.data).map(key => ({ key, value: response.data[key] }));
+                  setGroceries(groceriesData);
+                  setShoppingList(groceriesData.map(x => ({ key: x.key, value: x.value[0] })));
+               })
+               .catch((error) => {
+                  console.log(error);
+                  setShoppingList(null);
+               });
          });
 
    }, [])
 
-  
+
    return (
       <div style={{ color: 'white' }}>
          <h1 >Recepies</h1>
